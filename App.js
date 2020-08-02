740a4e94538229a3a01e3126fb1cab11837d664a
willpowr <hwillj@gmail.com>
2020-08-02 20:56:59 +0100

    Implemented reading of playlists json

--- App.js
+++ App.js
@@ -8,21 +8,63 @@ import {
 } from 'react-native';
 import { Portal } from 'react-native-paper';
 import { Provider as PaperProvider, Button, Dialog, Paragraph } from 'react-native-paper';
-import Constants from "expo-constants";
+import Constants from 'expo-constants'
+import LOCAL_PLAYLISTS_JSON from './assets/playlists/basic-playlists.json'
+
 
 const PROXY_SERVER = '192.168.1.8' + ':3000';
 const DEVICE_ID = "FF-01-25-79-C7-EC";
-const PLAYLISTS = ['Gym', 'Driving', 'Relax']
+const LOCAL_PLAYLISTS = LOCAL_PLAYLISTS_JSON.items //['Gym', 'Driving', 'Relax']
 
-function connectDms() {
-  console.log('Sending DMS Connect...')
+
+let tracks = []
+let inPlayerMode = false
+
+
+async function getLocalPlaylist(playlistPath) {
+  console.log('Playlist Data... ', playlistPath);
+  return {}
+}
+
+function requestPlaylist(isDeviceConnected, selectedPlaylist) {
+  isDeviceConnected ? getProviderPlaylist(selectedPlaylist.providerId, selectedPlaylist.playlistId) : getLocalPlaylist(selectedPlaylist.path)
+    .then(playlistData => {
+      console.log('Playlist Data... ', playlistData);
+      tracks = playlistData.items
+    });
+}
+
+function getProviderPlaylists() {
+  let playlistsRequest = new Request(`http://${PROXY_SERVER}/playlists/`);
+  let headers = playlistsRequest.headers;
+  headers.append('X-Audiowings-DeviceId', DEVICE_ID);
+  return fetch(playlistsRequest)
+    .then(status)
+    .then(response => response.json())
+    .catch(error => console.log(':( Request failed', error));
+}
+
+async function getLocalPlaylists() {
+  try{
+    return LOCAL_PLAYLISTS
+  }
+  catch (error) {
+    return console.log('Playlists File not found', error);
+  }
+}
+
+async function connectDms() {
   const connectRequest = new Request(`http://${PROXY_SERVER}/connect/`);
   const headers = connectRequest.headers;
-  headers.append('X-Audiowings-DeviceId', DEVICE_ID );
-  return fetch(connectRequest)
-    .then(response => response.json())
-    .then(userInfo => userInfo )
-    .catch(error => console.log('User not found', error));
+  headers.append('X-Audiowings-DeviceId', DEVICE_ID);
+  try {
+    const response = await fetch(connectRequest);
+    const userInfo = response.json();
+    return userInfo;
+  }
+  catch (error) {
+    return console.log('User not found', error);
+  }
 }
 
 export default function App() {
@@ -31,37 +73,39 @@ export default function App() {
 
   const [isSwitchOn, setIsSwitchOn] = React.useState(false);
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
-  
 
   useEffect(() => {
-    console.log('IN EFFECT [A] !!!!!')
     !isSwitchOn && setDeviceConnected(false)
-    isSwitchOn && !deviceConnected && connectDms()
+    isSwitchOn && !isDeviceConnected && connectDms()
       .then(userInfo => {
-        console.log(`User info: ${userInfo}`)
-        if(userInfo.deviceId === DEVICE_ID){
+        if (userInfo.deviceId === DEVICE_ID) {
           setDeviceConnected(true)
           setDeviceUser(userInfo)
         } else setIsSwitchOn(false)
       })
   });
-  useEffect(() => {
-    console.log('IN EFFECT [B] !!!!!')
-    console.log(`Device is ${deviceConnected ? '' : 'not '}connected`)
-  });
-
-  const [deviceConnected, setDeviceConnected] = React.useState(false);
 
 
+  const [isDeviceConnected, setDeviceConnected] = React.useState(false);
+
+  const [playlists, setPlaylists] = React.useState([{}])
 
   const [playlistsDialogVisible, setPlaylistsDialogVisible] = React.useState(false);
   const showDialog = () => {
     resetPlaylistIndex()
-    setPlaylistsDialogVisible(true)
+
+    isDeviceConnected ? getProviderPlaylists() : getLocalPlaylists()
+      .then(_playlists => {
+        setPlaylists(_playlists)
+        setPlaylistsDialogVisible(true)
+      })
   }
+
   const hideDialog = () => setPlaylistsDialogVisible(false)
+
   const onDialogYes = () => {
     setPlaylistsDialogVisible(false);
+    requestPlaylist(isDeviceConnected, playlists[selectedPlaylist])
   }
 
   const onDialogNo = () => {
@@ -69,13 +113,11 @@ export default function App() {
   }
 
   const _onLongPressButton = () => {
-    console.log('You long-pressed the button!')
     showDialog()
-
   }
 
   const [selectedPlaylist, setSelectedPlaylist] = React.useState(0);
-  const setNextPlaylist = () => setSelectedPlaylist(selectedPlaylist + 1 < PLAYLISTS.length ? selectedPlaylist + 1 : 0)
+  const setNextPlaylist = () => setSelectedPlaylist(selectedPlaylist + 1 < playlists.length ? selectedPlaylist + 1 : 0)
   const resetPlaylistIndex = () => setSelectedPlaylist(0);
 
   return (
@@ -83,8 +125,8 @@ export default function App() {
       <View style={styles.container}>
 
         <View style={styles.topBar}>
-          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} /*onClick={playmode && showDialog}*/ />
-          <Text style={styles.text}>{`${!deviceConnected ? 'dis' : JSON.stringify(deviceUser.displayname) + ' '}connected`}</Text>
+          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+          <Text style={styles.text}>{`${!isDeviceConnected ? 'Dis' : JSON.stringify(deviceUser.displayname) + ' '}connected`}</Text>
         </View>
 
         <View style={{ flex: 1 }}>
@@ -97,7 +139,7 @@ export default function App() {
           <Dialog visible={playlistsDialogVisible} onDismiss={hideDialog}>
             <Dialog.Title>Select Playlist</Dialog.Title>
             <Dialog.Content>
-              <Paragraph>{`Would you like playlist: ${PLAYLISTS[selectedPlaylist]}`}</Paragraph>
+              <Paragraph>{`Would you like playlist: ${playlists[selectedPlaylist].name}`}</Paragraph>
             </Dialog.Content>
             <Dialog.Actions>
               <Button onPress={onDialogYes}>Yes</Button>
